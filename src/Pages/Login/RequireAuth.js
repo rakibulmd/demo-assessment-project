61c2df394c7494b529304658fc8d18c099cfa140
Rakibul md <rakib245875@gmail.com>
2022-06-04 03:26:37 +0600

    Added: RequireAuth component to check whether logged in or not

---
+++ src/Pages/Login/RequireAuth.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Navigate, useLocation } from "react-router-dom";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
+
+const RequireAuth = ({ children }) => {
+    const [user, loading] = useAuthState(auth);
+    const location = useLocation();
+    if (loading) {
+        return (
+            <div className="flex justify-center items-center h-screen">
+                <div className="w-40 h-40 mx-auto">loading</div>
+            </div>
+        );
+    }
+    if (!user) {
+        return (
+            <Navigate to="/login" state={{ from: location }} replace></Navigate>
+        );
+    }
+
+    return children;
+};
+
+export default RequireAuth;
